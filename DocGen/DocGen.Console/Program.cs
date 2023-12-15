40d25914409cf131d3bcd5da7bd16cbe67285d2e
Constantine Korotky <const.korotky@outlook.com>
2023-12-15 22:22:29 +0200

    [add][word processing]

--- DocGen/DocGen.Console/Program.cs
+++ DocGen/DocGen.Console/Program.cs
@@ -6,6 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Office.Interop.Excel;
 using ExcelApplication = Microsoft.Office.Interop.Excel.Application;
+using WordApplication = Microsoft.Office.Interop.Word.Application;
 using DocGen.Processor;
 
 namespace DocGen.Console
@@ -14,8 +15,14 @@ namespace DocGen.Console
     {
         static void Main(string[] args)
         {
+            //ProcessExcel();
+            ProcessWord();
+        }
+
+        private static void ProcessExcel()
+        {
             var srcPath = @"D:\MSC\DG\examples.copy\Передок жовтень 2023.xlsx";
-            var destPath = $"D:\\MSC\\DG\\examples.copy\\res{DateTime.Now.Ticks}.xlsx";
+            var destPath = $"D:\\MSC\\DG\\examples.copy\\Excel_res{DateTime.Now.Ticks}.xlsx";
             var sheet = "жовтень";
             var range = "A2:AF124";
 
@@ -24,8 +31,21 @@ namespace DocGen.Console
             var entries = ExcelProcessor.ExtractEntries(app, srcPath, sheet, range);
             ExcelProcessor.PopulateEntries(app, destPath, sheet, entries);
 
-            /*var data = ExcelProcessor.ReadEntries(app, srcPath, sheet, range);
-            ExcelProcessor.WriteEntries(app, destPath, sheet, data);*/
+            /*var entries = ExcelProcessor.ReadEntries(app, srcPath, sheet, range);
+            ExcelProcessor.WriteEntries(app, destPath, sheet, entries);*/
+
+            app.Quit();
+            Marshal.FinalReleaseComObject(app);
+        }
+        private static void ProcessWord()
+        {
+            var srcPath = @"D:\MSC\DG\examples.copy\БН_НАЧПВЗ_37_041023.docx";
+            var destPath = $"D:\\MSC\\DG\\examples.copy\\Word_res{DateTime.Now.Ticks}.docx";
+
+            WordApplication app = new WordApplication();
+
+            var doc = WordProcessor.ReadEntries(app, srcPath);
+            WordProcessor.WriteEntries(app, doc, destPath);
 
             app.Quit();
             Marshal.FinalReleaseComObject(app);
--- DocGen/DocGen.Processor/WordProcessor.cs
+++ DocGen/DocGen.Processor/WordProcessor.cs
@@ -1,12 +1,118 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
+using System.Runtime.InteropServices.ComTypes;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
+using Microsoft.Office.Interop.Word;
+using WordApplication = Microsoft.Office.Interop.Word.Application;
+using WordDocument = Microsoft.Office.Interop.Word.Document;
 
 namespace DocGen.Processor
 {
     public class WordProcessor
     {
+        #region Raw Entries Read/Write
+
+        public static WordDocument ReadEntries(WordApplication app, string path)
+        {
+            WordDocument doc = app.Documents.Open(path, ReadOnly: true, Visible: false);
+            try
+            {
+                /*object findText = "А4765";
+                object replaceText = "############";
+
+                doc.Content.Find.Execute(
+                                 FindText: findText,
+                                 MatchCase: false,
+                                 MatchWholeWord: false,
+                                 MatchWildcards: false,
+                                 MatchSoundsLike: false,
+                                 MatchAllWordForms: false,
+                                 Forward: true, //this may be the one
+                                 Wrap: false,
+                                 Format: false,
+                                 ReplaceWith: replaceText,
+                                 Replace: WdReplace.wdReplaceAll
+                                 );*/
+
+            }
+            catch { }
+            return doc;
+        }
+
+        public static void WriteEntries(WordApplication app, WordDocument doc, string path)
+        {
+            var newDoc = app.Documents.Add();
+            try
+            {
+                doc.ActiveWindow.Selection.WholeStory();
+                doc.ActiveWindow.Selection.Copy();
+
+                //newDoc.Content.InsertAfter(doc.Content.Text.Replace("А4765", "###########"));
+                newDoc.ActiveWindow.Selection.Paste();
+                newDoc.Content.Find.Execute(
+                                 FindText: "А4765",
+                                 MatchCase: false,
+                                 MatchWholeWord: false,
+                                 MatchWildcards: false,
+                                 MatchSoundsLike: false,
+                                 MatchAllWordForms: false,
+                                 Forward: true, //this may be the one
+                                 Wrap: false,
+                                 Format: false,
+                                 ReplaceWith: "###########",
+                                 Replace: WdReplace.wdReplaceAll
+                                 );
+
+                newDoc.SaveAs(path);
+                newDoc.Close(true);
+                doc.Close(false);
+            }
+            catch { }
+            Marshal.FinalReleaseComObject(doc);
+            Marshal.FinalReleaseComObject(newDoc);
+        }
+
+        /// <summary>
+        /// DO NOT WORK
+        /// </summary>
+        public static void WriteEntriesContent(WordApplication app, WordDocument doc, string path)
+        {
+            var newDoc = app.Documents.Add();
+            try
+            {
+                doc.Content.Copy();
+                newDoc.Content.PasteSpecial(DataType: WdPasteOptions.wdKeepSourceFormatting);
+
+                newDoc.Content.Find.ClearFormatting();
+                newDoc.Content.Find.Replacement.ClearFormatting();
+
+                newDoc.Content.Find.Execute(
+                                 FindText: "А4765",
+                                 MatchCase: false,
+                                 MatchWholeWord: false,
+                                 MatchWildcards: false,
+                                 MatchSoundsLike: false,
+                                 MatchAllWordForms: false,
+                                 Forward: false, //this may be the one
+                                 Wrap: false,
+                                 Format: false,
+                                 ReplaceWith: "###########",
+                                 Replace: WdReplace.wdReplaceAll
+                                 );
+
+                newDoc.SaveAs(path);
+                newDoc.Close(true);
+                doc.Close(false);
+            }
+            catch { }
+            Marshal.FinalReleaseComObject(doc);
+            Marshal.FinalReleaseComObject(newDoc);
+        }
+
+        #endregion Raw Entries Read/Write
     }
 }
